0f6ceff7af42d42d439a639a8926c08a29602762
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-09-24 16:44:31 +0200

    Update : Edit and delete added

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@Angular/forms';
+import { ClientService } from '../../model/services/client.service';
+import { APIResponseModel } from '../../model/interface/role';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  clientObj: Client = new Client();
+  clientList: Client[] = [];
+  clientService = inject(ClientService);
 
+  ngOnInit(): void {
+    this.loadClient();
+  }
+
+  loadClient() {
+    this.clientService.getAllClients().subscribe((res: APIResponseModel) => {
+      this.clientList = res.data;
+    });
+  }
+
+  onSaveClient() {
+    debugger;
+    this.clientService
+      .addUpdate(this.clientObj)
+      .subscribe((res: APIResponseModel) => {
+        if (res.result) {
+          alert('Client created success');
+          this.loadClient();
+        } else {
+          alert(res.message);
+        }
+      });
+  }
+
+  onEdit(data:Client){
+    this.clientObj=data;
+  }
+
+  onDelete(id:number) {
+    const isDelete = confirm("Are you sure want to delete");
+    if(isDelete){
+      this.clientService
+        .deleteClientById(id)
+        .subscribe((res: APIResponseModel) => {
+          if (res.result) {
+            alert('Client deleted success');
+            this.loadClient();
+          } else {
+            alert(res.message);
+          }
+        });
+    }
+  }
 }
