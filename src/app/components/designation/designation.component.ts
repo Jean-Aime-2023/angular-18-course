4c4aeaaf66500f218f3b41e74158616bc5d40153
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-09-24 08:49:22 +0200

    Updates : Control Flow Statement

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../model/services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
   standalone: true,
   imports: [],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
-
+export class DesignationComponent implements OnInit {
+  designationList:IDesignation[]=[]
+  isLoader:boolean = true;
+  masterService = inject(MasterService);
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe((result: APIResponseModel) => {
+      this.designationList=result.data;
+    },error=>{
+      alert("API error / Network Down")
+    });
+  }
 }
--- src/app/model/interface/role.ts
+++ src/app/model/interface/role.ts
@@ -2,3 +2,14 @@ export interface IRole{
     roleId: number,
     role: string
 }
+
+export interface IDesignation {
+    designation:string,
+    designationId:number
+}
+
+export interface APIResponseModel{
+    message:string,
+    result:boolean,
+    data:any
+}
