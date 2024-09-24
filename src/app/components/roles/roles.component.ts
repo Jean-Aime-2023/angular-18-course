9c260cad0e1dcb02d517fa746cd6364c1617f443
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-09-24 05:58:10 +0200

    Update : Data Binding

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@Angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css',
+})
+export class RolesComponent {
+  // types of variables
+  // string , number , boolean,date,object,array,null,undefined
+  firstName: string = 'Angular Tutorial';
+  angularVersion = 'Version 18';
+  version: number = 18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = 'button';
+  selectedState: string = '';
+}
